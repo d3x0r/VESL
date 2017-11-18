f451ad0fd80cae12961c014fd40ecf5762f6bf0c
d3x0r <d3x0r@users.noreply.github.com>
2017-11-18 10:44:28 -0800

    Reformat and remove VRControls update

--- three.setup.js
+++ three.setup.js
@@ -91,6 +91,7 @@ function setControls3() {
 
 
 var status_line;
+
 	function init() {
 		document.getElementById( "controls1").onclick = setControls1;
 		document.getElementById( "controls2").onclick = setControls2;
@@ -105,8 +106,6 @@ var status_line;
 		camera.matrixAutoUpdate = false;
 		camera.position.z = 5;
 		camera.matrixWorldNeedsUpdate = true;
-
-		//geometryShader = new THREE.MeshBasicMaterial();
 
 		 // for phong hello world test....
  		var light = new THREE.PointLight( 0xffFFFF, 1, 1000 );
@@ -140,8 +139,10 @@ var status_line;
 //		          return;
 //		        }
 
-		
+
 		document.body.appendChild( renderer.domElement );
+		if ( typeof WEBVR !== "undefined" )
+			document.body.appendChild( WEBVR.createButton( renderer ) )
 
 		if( THREE.NaturalControls ) {
 			controlNatural = new THREE.NaturalControls( camera, renderer.domElement );
@@ -160,119 +161,113 @@ var status_line;
 
 		if( VR ) {
 
-function handleController( controller ) {
-  // update controller object from VR input (gamepad)
-	controller.update();
+			function handleController( controller ) {
+				// update controller object from VR input (gamepad)
+				controller.update();
 
-	var pivot = controller.getObjectByName( 'pivot' );
+				var pivot = controller.getObjectByName( 'pivot' );
 
-	if ( pivot ) {
+				if ( pivot ) {
 
-		pivot.material.color.copy( controller.getColor() );
+					pivot.material.color.copy( controller.getColor() );
 
-		var matrix = pivot.matrixWorld;
+					var matrix = pivot.matrixWorld;
 
-		var point1 = controller.userData.points[ 0 ];
-		var point2 = controller.userData.points[ 1 ];
+					var point1 = controller.userData.points[ 0 ];
+					var point2 = controller.userData.points[ 1 ];
 
-		var matrix1 = controller.userData.matrices[ 0 ];
-		var matrix2 = controller.userData.matrices[ 1 ];
+					var matrix1 = controller.userData.matrices[ 0 ];
+					var matrix2 = controller.userData.matrices[ 1 ];
 
-		point1.setFromMatrixPosition( matrix );
-		matrix1.lookAt( point2, point1, THREE.Vector3Up );
+					point1.setFromMatrixPosition( matrix );
+					matrix1.lookAt( point2, point1, THREE.Vector3Up );
 
-		if ( controller.getButtonState( 'trigger' ) ) {
+					if ( controller.getButtonState( 'trigger' ) ) {
 
-      // this is the old drawing code...
-			//stroke( controller, point1, point2, matrix1, matrix2 );
+						// this is the old drawing code...
+						//stroke( controller, point1, point2, matrix1, matrix2 );
 
-		}
+					}
 
-		point2.copy( point1 );
-		matrix2.copy( matrix1 );
+					point2.copy( point1 );
+					matrix2.copy( matrix1 );
 
-	}
+				}
 
-}
-
-
-function setupViveControls( scene ) {
-
-		// update camera poosition from VR inputs...
-	//if( Voxelarium.Settings.AltSpace ) {
-	//	controls = new THREE.AltSpaceControls( camera );
-	//} else
-	//effect = new THREE.VREffect( renderer );
-	//effect = new THREE.StereoEffect( renderer );
-	//effect.autoSubmitFrame = false;
-	//effect.autoClear = true;
-	//effect.setSize( window.innerWidth*4/5, window.innerHeight*4/5 );
-
-	 if ( typeof WEBVR !== "undefined" )
-		document.body.appendChild( WEBVR.createButton( renderer ) )
+			}
 
 
+			function setupViveControls( scene ) {
 
-	controls = new THREE.VRControls( camera );
-	controls.standing = true;
+					// update camera poosition from VR inputs...
+				//if( Voxelarium.Settings.AltSpace ) {
+				//	controls = new THREE.AltSpaceControls( camera );
+				//} else
+				//effect = new THREE.VREffect( renderer );
+				//effect = new THREE.StereoEffect( renderer );
+				//effect.autoSubmitFrame = false;
+				//effect.autoClear = true;
+				//effect.setSize( window.innerWidth*4/5, window.innerHeight*4/5 );
 
-	scene.add( new THREE.HemisphereLight( 0x888877, 0x777788 ) );
+				//controls = new THREE.VRControls( camera );
+				//controls.standing = true;
 
-	var headLight = new THREE.DirectionalLight( 0xffffff );
-	headLight.position.set( 0, 6, 0 );
-	headLight.castShadow = true;
-	headLight.shadow.camera.top = 2;
-	headLight.shadow.camera.bottom = -2;
-	headLight.shadow.camera.right = 2;
-	headLight.shadow.camera.left = -2;
-	headLight.shadow.mapSize.set( 4096, 4096 );
-	scene.add( headLight );
+				scene.add( new THREE.HemisphereLight( 0x888877, 0x777788 ) );
 
+				var headLight = new THREE.DirectionalLight( 0xffffff );
+				headLight.position.set( 0, 6, 0 );
+				headLight.castShadow = true;
+				headLight.shadow.camera.top = 2;
+				headLight.shadow.camera.bottom = -2;
+				headLight.shadow.camera.right = 2;
+				headLight.shadow.camera.left = -2;
+				headLight.shadow.mapSize.set( 4096, 4096 );
+				scene.add( headLight );
 
-	// controllers
+				// controllers
+				var controller1 = new VESL.ViveController( 0 );
+				controller1.standingMatrix = controls.getStandingMatrix();
+				controller1.userData.points = [ new THREE.Vector3(), new THREE.Vector3() ];
+				controller1.userData.matrices = [ new THREE.Matrix4(), new THREE.Matrix4() ];
+				controller1.userData.altspace = { collider: { enabled: false } };
+				scene.add( controller1 );
 
-	var controller1 = new VESL.ViveController( 0 );
-	controller1.standingMatrix = controls.getStandingMatrix();
-	controller1.userData.points = [ new THREE.Vector3(), new THREE.Vector3() ];
-	controller1.userData.matrices = [ new THREE.Matrix4(), new THREE.Matrix4() ];
-	controller1.userData.altspace = { collider: { enabled: false } };
-	scene.add( controller1 );
+				var controller2 = new VESL.ViveController( 1 );
+				controller2.standingMatrix = controls.getStandingMatrix();
+				controller2.userData.points = [ new THREE.Vector3(), new THREE.Vector3() ];
+				controller2.userData.matrices = [ new THREE.Matrix4(), new THREE.Matrix4() ];
+				controller2.userData.altspace = { collider: { enabled: false } };
+				scene.add( controller2 );
 
-	var controller2 = new VESL.ViveController( 1 );
-	controller2.standingMatrix = controls.getStandingMatrix();
-	controller2.userData.points = [ new THREE.Vector3(), new THREE.Vector3() ];
-	controller2.userData.matrices = [ new THREE.Matrix4(), new THREE.Matrix4() ];
-	controller2.userData.altspace = { collider: { enabled: false } };
-	scene.add( controller2 );
+				var loader = new THREE.OBJLoader();
+				loader.setPath( 'models/obj/vive-controller/' );
+				loader.load( 'vr_controller_vive_1_5.obj', function ( object ) {
+					var loader = new THREE.TextureLoader();
+					loader.setPath( 'models/obj/vive-controller/' );
 
-	var loader = new THREE.OBJLoader();
-	loader.setPath( 'models/obj/vive-controller/' );
-	loader.load( 'vr_controller_vive_1_5.obj', function ( object ) {
-		var loader = new THREE.TextureLoader();
-		loader.setPath( 'models/obj/vive-controller/' );
+					var controller = object.children[ 0 ];
+					controller.material.map = loader.load( 'onepointfive_texture.png' );
+					controller.material.specularMap = loader.load( 'onepointfive_spec.png' );
+					controller.castShadow = true;
+					controller.receiveShadow = true;
 
-		var controller = object.children[ 0 ];
-		controller.material.map = loader.load( 'onepointfive_texture.png' );
-		controller.material.specularMap = loader.load( 'onepointfive_spec.png' );
-		controller.castShadow = true;
-		controller.receiveShadow = true;
+					// var pivot = new THREE.Group();
+					// var pivot = new THREE.Mesh( new THREE.BoxGeometry( 0.01, 0.01, 0.01 ) );
+					var pivot = new THREE.Mesh( new THREE.IcosahedronGeometry( 0.002, 2 ) );
+					pivot.name = 'pivot';
+					pivot.position.y = -0.016;
+					pivot.position.z = -0.043;
+					pivot.rotation.x = Math.PI / 5.5;
+					controller.add( pivot );
 
-		// var pivot = new THREE.Group();
-		// var pivot = new THREE.Mesh( new THREE.BoxGeometry( 0.01, 0.01, 0.01 ) );
-		var pivot = new THREE.Mesh( new THREE.IcosahedronGeometry( 0.002, 2 ) );
-		pivot.name = 'pivot';
-		pivot.position.y = -0.016;
-		pivot.position.z = -0.043;
-		pivot.rotation.x = Math.PI / 5.5;
-		controller.add( pivot );
+					controller1.add( controller.clone() );
 
-		controller1.add( controller.clone() );
+					pivot.material = pivot.material.clone();
+					controller2.add( controller.clone() );
+				} );
+			}
+			setupViveControls( scene );
 
-		pivot.material = pivot.material.clone();
-		controller2.add( controller.clone() );
-	} );
-}
-		setupViveControls( scene );
 		}
 		else {
 			controls = controlNatural;
@@ -297,8 +292,8 @@ function beginAnimate()  {
 
 function render() {
 	var delta = clock.getDelta();
-	//if( controls )
-	controls.update(delta);
+	if( controls )
+		controls.update(delta);
 
 	if( controller1 ) {
 		handleController( controller1 );
